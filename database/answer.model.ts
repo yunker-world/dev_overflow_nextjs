625612ee7bd5b8d5d64eb28db2ee6b7071a704bc
yunker-world <yunker.world@gmail.com>
2023-11-12 21:53:28 +0800

    fix: no-unused-vars

--- database/answer.model.ts
+++ database/answer.model.ts
@@ -11,7 +11,7 @@ export interface IAnswer extends Document {
 
 const AnswerSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  question: { type: Schema.Types.ObjectId, ref: "Question", required: treu },
+  question: { type: Schema.Types.ObjectId, ref: "Question", required: true },
   content: { type: String, required: true },
   upvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
   downvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
