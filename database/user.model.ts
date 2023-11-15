b80a3ed75053d2d2ec4d44a549764265984ab9ab
yunker-world <yunker.world@gmail.com>
2023-11-15 09:07:47 +0800

    fix

--- database/user.model.ts
+++ database/user.model.ts
@@ -24,7 +24,7 @@ const UserSchema = new Schema({
   bio: { type: String },
   picture: { type: String, required: true },
   location: { type: String },
-  portfolioWebsite: { type: String, required: true },
+  portfolioWebsite: { type: String },
   reputation: { type: Number, default: 0 },
   saved: { type: Schema.Types.ObjectId, ref: "Question" },
   joinedAt: { type: Date, default: Date.now },
